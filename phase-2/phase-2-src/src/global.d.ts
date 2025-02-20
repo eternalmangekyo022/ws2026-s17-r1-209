1fd3ad0f29558a375c9631934db66ed768b94ad2
Tibor Fazekas <tibcso0322@gmail.com>
2025-02-20 01:10:36 +0100

    added types for tiles

--- phase-2/phase-2-src/src/global.d.ts
+++ phase-2/phase-2-src/src/global.d.ts
@@ -16,4 +16,33 @@ declare global {
     from: string;
     to: string;
   }
+
+  type ITileFormats =
+    | "waiting"
+    | "table"
+    | "entrance"
+    | "wall"
+    | "washer"
+    | "dryer";
+
+  interface IGeneralTile {
+    type: Omit<ITileFormats, "dryer" | "washer">;
+  }
+
+  interface IWasher {
+    type: "washer";
+    weight: 8 | 11;
+  }
+
+  interface IDryer {
+    type: "dryer";
+    weight: 18 | 25;
+  }
+
+  type Tile = {
+    id: number;
+    pos: {
+      [keyof("x" | "y")]: number;
+    };
+  } & (IGeneralTile | IWasher | IDryer);
 }
